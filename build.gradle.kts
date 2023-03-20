277a46ee7d7c6001631948a51f54872c57295bf1
Midka <me@midka.dev>
2023-03-20 18:22:28 +0200

    server starts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,25 @@
 plugins {
     kotlin("jvm") version "1.8.0"
-    application
+    kotlin("plugin.serialization") version "1.6.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("org.ajoberstar.grgit") version "4.1.0"
 }
 
 group = "dev.midka"
 version = "1.0-SNAPSHOT"
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xinline-classes")
+    jvmTarget = "17"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
+}
 
 repositories {
     mavenCentral()
@@ -15,17 +30,33 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("com.akuleshov7:ktoml-core:0.4.1")
     implementation("com.akuleshov7:ktoml-file:0.4.1")
-    implementation("com.github.Minestom:Minestom:067227421f")
+    implementation("com.github.Minestom.Minestom:Minestom:067227421f")
+}
+
+tasks {
+    processResources {
+        filesMatching("META-INF/*") {
+            filter {
+                it.replace("{git.commit}", grgit.head().id)
+            }
+        }
+    }
+
+    shadowJar {
+        manifest {
+            attributes (
+                "Main-Class" to "dev.midka.astom.Server",
+                "Multi-Release" to true
+            )
+        }
+
+        archiveBaseName.set(project.name)
+    }
+
+    test { useJUnitPlatform() }
+    build { dependsOn(shadowJar) }
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(8)
-}
-
-application {
-    mainClass.set("MainKt")
 }
--- src/main/kotlin/dev/midka/astom/Astom.kt
+++ src/main/kotlin/dev/midka/astom/Astom.kt
@@ -1,13 +1,16 @@
 package dev.midka.astom
 
-import com.akuleshov7.ktoml.Toml.Default.inputConfig
-import com.akuleshov7.ktoml.TomlInputConfig
 import com.akuleshov7.ktoml.TomlOutputConfig
 import com.akuleshov7.ktoml.file.TomlFileReader
 import com.akuleshov7.ktoml.file.TomlFileWriter
 import dev.midka.astom.config.Configuration
 import kotlinx.serialization.serializer
 import java.io.File
+import net.minestom.server.MinecraftServer
+import net.minestom.server.extras.MojangAuth
+import net.minestom.server.extras.PlacementRules
+import net.minestom.server.extras.bungee.BungeeCordProxy
+import net.minestom.server.extras.velocity.VelocityProxy
 
 object Astom {
     fun start(callback: AstomContext.() -> Unit = {}) {
@@ -25,5 +28,20 @@ object Astom {
 
         System.setProperty("minestom.chunk-view-distance", configuration.chunkViewDistance.toString())
         System.setProperty("minestom.entity-view-distance", configuration.entityViewDistance.toString())
+
+        val minecraftServer = MinecraftServer.init()
+        MinecraftServer.setBrandName("Astom (Minestom)")
+
+        if (configuration.mojangAuth) MojangAuth.init()
+        if (configuration.bungeeConfiguration.enabled) BungeeCordProxy.enable()
+        if (configuration.velocityConfiguration.enabled) VelocityProxy.enable(configuration.velocityConfiguration.secretKey)
+
+        PlacementRules.init()
+
+        with(context) {
+            callback()
+        }
+
+        minecraftServer.start(configuration.host, configuration.port)
     }
 }
