a649dc283a1d2033a24ccb9c8bc7502c46ce5d87
Midka <me@midka.dev>
2023-03-20 19:52:16 +0200

    make path private

--- src/main/kotlin/dev/midka/astom/utils/Versioning.kt
+++ src/main/kotlin/dev/midka/astom/utils/Versioning.kt
@@ -1,6 +1,6 @@
 package dev.midka.astom.utils
 
-class Versioning(val path: String) {
+class Versioning(private val path: String) {
     fun get(): String {
         return javaClass.getResource(path)?.readText()?.trim()?.substring(0..15) ?: "Unknown"
     }
