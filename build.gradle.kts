9eef86a80ec50ce80c2dfb1781468bcb3b5d17d5
Midka <me@midka.dev>
2023-03-20 20:00:04 +0200

    refactor: build.gradle.kts, thanks @bluelhf

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,26 +8,12 @@ plugins {
 group = "dev.midka"
 version = "1.0-SNAPSHOT"
 
-val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    freeCompilerArgs = listOf("-Xinline-classes")
-    jvmTarget = "17"
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-    withSourcesJar()
-    withJavadocJar()
-}
-
 repositories {
     mavenCentral()
     maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("com.akuleshov7:ktoml-core:0.4.1")
     implementation("com.akuleshov7:ktoml-file:0.4.1")
     implementation("com.github.Minestom.Minestom:Minestom:067227421f")
@@ -54,10 +40,14 @@ tasks {
         archiveBaseName.set(project.name)
     }
 
-    test { useJUnitPlatform() }
     build { dependsOn(shadowJar) }
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+    withSourcesJar()
+    withJavadocJar()
 }
